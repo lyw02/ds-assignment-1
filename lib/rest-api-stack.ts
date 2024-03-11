7974ecd5cb4449ab321acd6f8e2c72721d2525bd
Yiwei Liu <2814824250@qq.com>
2024-03-11 23:59:28 +0000

    Lambda - get all movie reviews

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -6,6 +6,7 @@ import * as custom from 'aws-cdk-lib/custom-resources';
 import {Construct} from 'constructs';
 import {generateBatch} from '../shared/util';
 import {movieReviews} from '../seed/movieReviews';
+import * as apig from 'aws-cdk-lib/aws-apigateway';
 
 export class RestAPIAssignmentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,10 +16,27 @@ export class RestAPIAssignmentStack extends cdk.Stack {
     const movieReviewsTable = new dynamodb.Table(this, 'MovieReviewsTable', {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: {name: 'movieId', type: dynamodb.AttributeType.NUMBER},
-      sortKey: { name: 'reviewerName', type: dynamodb.AttributeType.STRING },
+      sortKey: {name: 'reviewerName', type: dynamodb.AttributeType.STRING},
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       tableName: 'MovieReviews',
     });
+
+    // Functions
+    const getAllMovieReviewsFn = new lambdanode.NodejsFunction(
+      this,
+      'GetAllMovieReviewsFn',
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/getAllMovieReviews.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: movieReviewsTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      },
+    );
 
     new custom.AwsCustomResource(this, 'moviesddbInitData', {
       onCreate: {
@@ -29,13 +47,36 @@ export class RestAPIAssignmentStack extends cdk.Stack {
             [movieReviewsTable.tableName]: generateBatch(movieReviews),
           },
         },
-        physicalResourceId: custom.PhysicalResourceId.of(
-          'moviesddbInitData',
-        ), //.of(Date.now().toString()),
+        physicalResourceId: custom.PhysicalResourceId.of('moviesddbInitData'), //.of(Date.now().toString()),
       },
       policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
         resources: [movieReviewsTable.tableArn],
       }),
     });
+
+    // Permissions
+    movieReviewsTable.grantReadData(getAllMovieReviewsFn);
+
+    // REST API
+    const api = new apig.RestApi(this, 'RestApi', {
+      description: 'Movie reviews API',
+      deployOptions: {
+        stageName: 'dev',
+      },
+      defaultCorsPreflightOptions: {
+        allowHeaders: ['Content-Type', 'X-Amz-Date'],
+        allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowCredentials: true,
+        allowOrigins: ['*'],
+      },
+    });
+
+    // Endpoints
+    const moviesEndpoint = api.root.addResource('movies');
+    // const reviewsEndpoint = moviesEndpoint.addResource('reviews');
+    moviesEndpoint.addMethod(
+      'GET',
+      new apig.LambdaIntegration(getAllMovieReviewsFn, {proxy: true}),
+    );
   }
 }

f327bf0218230afb0b6fa873f9782b368a757917
Yiwei Liu <2814824250@qq.com>
2024-03-12 02:03:55 +0000

    Extend get reviews lambda to allow query params (minRating)

--- lambdas/getAllMovieReviews.ts
+++ lambdas/getAllMovieReviews.ts
@@ -1,41 +1,95 @@
-import {Handler} from 'aws-lambda';
+import {APIGatewayProxyHandlerV2} from 'aws-lambda';
 
 import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
-import {DynamoDBDocumentClient, ScanCommand} from '@aws-sdk/lib-dynamodb';
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  ScanCommandInput,
+} from '@aws-sdk/lib-dynamodb';
+import Ajv from 'ajv';
+import schema from '../shared/types.schema.json';
+
+const ajv = new Ajv();
+const isValidQueryParams = ajv.compile(
+  schema.definitions['MovieReviewsQueryParams'] || {},
+);
 
 const ddbDocClient = createDDbDocClient();
 
-export const handler: Handler = async (event, context) => {
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
   try {
     console.log('Event: ', event);
 
-    const commandOutput = await ddbDocClient.send(
-      new ScanCommand({
-        TableName: process.env.TABLE_NAME,
-      }),
-    );
+    const queryParams = event.queryStringParameters;
 
-    if (!commandOutput.Items) {
+    if (queryParams && !isValidQueryParams(queryParams)) {
+      // Invalid query params
       return {
-        statusCode: 404,
+        statusCode: 500,
         headers: {
           'content-type': 'application/json',
         },
-        body: JSON.stringify({Message: 'Invalid movie Id'}),
+        body: JSON.stringify({Message: 'Invalid min rating'}),
+      };
+    } else if (
+      queryParams &&
+      isValidQueryParams(queryParams) &&
+      queryParams.minRating
+    ) {
+      // Valid query params
+      const minRating = parseInt(queryParams.minRating);
+
+      let commandInput: ScanCommandInput = {
+        TableName: process.env.TABLE_NAME,
+        FilterExpression: 'rating >= :mr',
+        ExpressionAttributeValues: {
+          ':mr': minRating,
+        },
+      };
+
+      const commandOutput = await ddbDocClient.send(
+        new ScanCommand(commandInput),
+      );
+
+      return {
+        statusCode: 200,
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          data: commandOutput.Items,
+        }),
+      };
+    } else {
+      // No query params
+      const commandOutput = await ddbDocClient.send(
+        new ScanCommand({
+          TableName: process.env.TABLE_NAME,
+        }),
+      );
+
+      if (!commandOutput.Items) {
+        return {
+          statusCode: 404,
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({Message: 'Error'}),
+        };
+      }
+
+      const body = {
+        data: commandOutput.Items,
+      };
+
+      return {
+        statusCode: 200,
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(body),
       };
     }
-
-    const body = {
-      data: commandOutput.Items,
-    };
-
-    return {
-      statusCode: 200,
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    };
   } catch (error: any) {
     console.log(JSON.stringify(error));
 
